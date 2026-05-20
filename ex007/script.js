0c82c91b535d7ae18aed9df757ca4f20e3598852
Heinsenberg <souzagessi82@gmail.com>
2026-05-20 20:14:55 -0300

    Alterações
    Alterações na declaração do ELSE.

--- ex007/script.js
+++ ex007/script.js
@@ -1,12 +1,14 @@
 var numero_1 = 2; 
-var numero_2 = 10;
+var numero_2 = 26;
 var numero_3 = 26;
 
 
 if ((numero_1 >= numero_2) && (numero_3 <= numero_1)) {
-    console.log(`O mior númeor é ${numero_1}`);
+    console.log(`O maior número é ${numero_1}`);
 } else if ((numero_2 >= numero_1) && (numero_3 <= numero_2)) {
-    console.log(`O Maior mpumero é ${numero_2}`);
+    console.log(`O maior número é ${numero_2}`);
+} else if ((numero_3 >= numero_2) && (numero_1 <= numero_3)) {
+    console.log(`O maior número é ${numero_3}`);
 } else {
-    console.log(`O MAior número é ${numero_3}`);
+    console.log();
 }
